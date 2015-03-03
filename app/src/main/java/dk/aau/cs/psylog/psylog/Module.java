dfc3994ea470341060b1f3e7479fc0bf53de7414
Bruno Thalmann <mr.thalmann@gmail.com>
2015-03-03 09:28:23 +0100

    added list of tables

--- app/src/main/java/dk/aau/cs/psylog/psylog/Module.java
+++ app/src/main/java/dk/aau/cs/psylog/psylog/Module.java
@@ -1,7 +1,23 @@
 package dk.aau.cs.psylog.psylog;
 
-/**
- * Created by - on 02-03-2015.
- */
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Module {
+    public final String name;
+    public final String version;
+    private List<Table> tables;
+
+    public Module(String name, String version)
+    {
+        this.name = name;
+        this.version = version;
+        tables = new ArrayList<Table>();
+    }
+
+    public void AddTable(Table table)
+    {
+        this.tables.add(table);
+    }
 }
