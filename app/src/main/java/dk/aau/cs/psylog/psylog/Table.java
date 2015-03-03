7a55979a93d67176f96be20916346dcc9a9a0e37
Bruno Thalmann <mr.thalmann@gmail.com>
2015-03-03 09:28:38 +0100

    class representing table

---
+++ app/src/main/java/dk/aau/cs/psylog/psylog/Table.java
@@ -0,0 +1,15 @@
+package dk.aau.cs.psylog.psylog;
+
+/**
+ * Created by - on 03-03-2015.
+ */
+public class Table {
+    public final String name;
+    public final String frequency;
+
+    public Table(String name, String frequency)
+    {
+        this.name = name;
+        this.frequency = frequency;
+    }
+}
