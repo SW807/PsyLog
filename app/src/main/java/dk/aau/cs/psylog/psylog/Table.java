9194e472bf31264acbae1e453645bb55cd980c92
Bruno Thalmann <mr.thalmann@gmail.com>
2015-03-03 09:52:58 +0100

    added list of columns to table

--- app/src/main/java/dk/aau/cs/psylog/psylog/Table.java
+++ app/src/main/java/dk/aau/cs/psylog/psylog/Table.java
@@ -1,15 +1,29 @@
 package dk.aau.cs.psylog.psylog;
 
-/**
- * Created by - on 03-03-2015.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class Table {
     public final String name;
     public final String frequency;
+    private List<Column> columns;
 
     public Table(String name, String frequency)
     {
         this.name = name;
         this.frequency = frequency;
+        this.columns = new ArrayList<Column>();
+    }
+
+    public Table(String name)
+    {
+        this.name = name;
+        this.frequency = null;
+        this.columns = new ArrayList<Column>();
+    }
+
+    public void AddColumn(Column column)
+    {
+        this.columns.add(column);
     }
 }
