3bce87e288fa28006ec4272d29e0d39b2dd5c2f4
Bruno Thalmann <mr.thalmann@gmail.com>
2015-03-04 12:02:48 +0100

    XMLparser festivitas

---
+++ app/src/main/java/dk/aau/cs/psylog/generated/Test.java
@@ -0,0 +1,25 @@
+package dk.aau.cs.psylog.generated;
+
+/**
+ * Created by - on 04-03-2015.
+ */
+public class Test {
+    public int getBruno() {
+        return bruno;
+    }
+
+    public void setBruno(int bruno) {
+        this.bruno = bruno;
+    }
+
+    public int getMikael() {
+        return mikael;
+    }
+
+    public void setMikael(int mikael) {
+        this.mikael = mikael;
+    }
+
+    int mikael;
+    int bruno;
+}
--- app/src/main/java/dk/aau/cs/psylog/psylog/XMLParser.java
+++ app/src/main/java/dk/aau/cs/psylog/psylog/XMLParser.java
@@ -12,13 +12,21 @@ import com.fasterxml.jackson.dataformat.xml.JacksonXmlModule;
 import com.fasterxml.jackson.dataformat.xml.XmlFactory;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
+import org.codehaus.stax2.util.StreamWriter2Delegate;
+
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.util.HashMap;
+
 import dk.aau.cs.psylog.generated.Module;
+import dk.aau.cs.psylog.generated.Tables;
+import dk.aau.cs.psylog.generated.Test;
 
 public class XMLParser {
 
     public XMLParser(Context context)
     {
-        try {
+        //try {
             //Use Aalto StAX implementation explicitly for XML parsing
             XmlFactory f = new XmlFactory(new InputFactoryImpl(), new OutputFactoryImpl());
 
@@ -33,19 +41,37 @@ public class XMLParser {
 
             XmlMapper xmlMapper = new XmlMapper(f, module);
 
+
+
             xmlMapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
             xmlMapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT,true);
             xmlMapper.configure(DeserializationFeature.USE_JAVA_ARRAY_FOR_JSON_ARRAY, true);
             xmlMapper.configure(DeserializationFeature.READ_ENUMS_USING_TO_STRING,true);
 
+
             //Tell Jackson to expect the XML in PascalCase, instead of camelCase
-            xmlMapper.setPropertyNamingStrategy(new PropertyNamingStrategy.PascalCaseStrategy());
+            //xmlMapper.setPropertyNamingStrategy(new PropertyNamingStrategy.PascalCaseStrategy());
+
 
             //Make the HTTP request, and deserialize the XML response into the Siri object
-            Module m = xmlMapper.readValue(ServiceHelper.getXMLForInstalledProcesses(context).get("dk.aau.cs.psylog.psylog_accelerometermodule"),  Module.class);
-        } catch(Exception e){
-            Log.e("HEJ STUFFE!", "Error fetching or parsing XML: " + e);
-        }
-    }
+        try {
+            HashMap<String,InputStream> k = ServiceHelper.getXMLForInstalledProcesses(context);
+            InputStream crap = k.get("dk.aau.cs.psylog.psylog_accelerometermodule");
 
+
+            String s = new java.util.Scanner(crap).useDelimiter("\\A").next();
+
+            Log.d("storlog",s);
+
+            //Module m = xmlMapper.readValue(crap, Module.class);
+
+            //Test test = xmlMapper.readValue("<test><bruno>1</bruno><mikael>5</mikael></test>",Test.class);
+            Test test = xmlMapper.readValue("<test><bruno>1</bruno><mikael>5</mikael></test>",Test.class);
+            test.getBruno();
+        }
+        catch (Exception e){
+        Log.d("sick", "FUCKJ - " + e.getMessage());
+    }
+    }
 }
+
