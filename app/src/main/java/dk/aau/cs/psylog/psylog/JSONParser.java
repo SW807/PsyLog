eb699a20d51f2ec4851366b6298585563d89947d
Bruno Thalmann <mr.thalmann@gmail.com>
2015-03-05 11:35:45 +0100

    added functions for parsing objects and creating tables

--- app/src/main/java/dk/aau/cs/psylog/psylog/JSONParser.java
+++ app/src/main/java/dk/aau/cs/psylog/psylog/JSONParser.java
@@ -3,17 +3,28 @@ package dk.aau.cs.psylog.psylog;
 import android.content.Context;
 import android.util.Log;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 import dk.aau.cs.psylog.generated.Module;
+import dk.aau.cs.psylog.generated.Table;
 
 public class JSONParser {
 
+    private Context context;
+
     JSONParser(Context context)
     {
+        this.context = context;
         ObjectMapper mapper = new ObjectMapper();
 
         try
@@ -23,10 +34,46 @@ public class JSONParser {
 
             Module test = mapper.readValue(is, Module.class);
             Log.d("test", test.getName());
+            test.getTables();
         }
         catch(Exception alt)
         {
             Log.d("sick",alt.getMessage());
         }
+
     }
+
+    private List<Module> parse()
+    {
+        List<Module> modules = new ArrayList<>();
+        ObjectMapper mapper = new ObjectMapper();
+        HashMap<String, InputStream> processes = ServiceHelper.getJSONForInstalledProcesses(context);
+        for(InputStream is : processes.values())
+        {
+            try {
+                modules.add(mapper.readValue(is, Module.class));
+            }
+            catch (IOException e) {
+                Log.e("JSONParser", e.getMessage());
+            }
+        }
+        return  modules;
+    }
+
+    private List<String> getTables(Module module)
+    {
+        List<String> l = new ArrayList<>();
+        for(Table t : module.getTables())
+            l.add(t.getName());
+        return l;
+    }
+
+    
+
+    private void createTables()
+    {
+        SQLiteHelper sqLiteHelper = new SQLiteHelper(context);
+        sqLiteHelper.createTable();
+    }
+
 }
