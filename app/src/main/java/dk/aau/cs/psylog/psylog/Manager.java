ebe2ed02cc8fa6428440a21a8a3578087ffbabcf
Bruno Thalmann <mr.thalmann@gmail.com>
2015-03-06 10:56:10 +0100

    method for updating table entry in moduleVersion table

--- app/src/main/java/dk/aau/cs/psylog/psylog/Manager.java
+++ app/src/main/java/dk/aau/cs/psylog/psylog/Manager.java
@@ -27,9 +27,28 @@ public class Manager {
         sqLiteHelper.createTable(context.getString(R.string.Manager_ModuleVersionsTable), new String[]{context.getString(R.string.Manager_ModuleVersionsTable_NameColumn), context.getString(R.string.Manager_ModuleVersionsTable_VersionColumn)});
     }
 
-    private void updateModuleVersions(String name, String version)
+    private void updateModuleVersion(String name, String version) throws SQLDataException
     {
-        
+        Cursor cursor = sqLiteHelper.readFromDB(context.getString(R.string.Manager_ModuleVersionsTable), null, context.getString(R.string.Manager_ModuleVersionsTable_NameColumn) + " = ?", new String[]{name}, null, null, null, null);
+        if (cursor.getCount() == 1)
+        {
+            ContentValues cv = new ContentValues();
+            cv.put(context.getString(R.string.Manager_ModuleVersionsTable_VersionColumn),version);
+            int affected = sqLiteHelper.updateDB(context.getString(R.string.Manager_ModuleVersionsTable), cv, context.getString(R.string.Manager_ModuleVersionsTable_NameColumn) + " = ?", new String[]{name});
+            if(affected != 1)
+                throw new InternalError("Duplicate module in modules table - RIP Project");
+        }
+        else if (cursor.getCount() == 0)
+        {
+            ContentValues cv = new ContentValues();
+            cv.put(context.getString(R.string.Manager_ModuleVersionsTable_NameColumn),name);
+            cv.put(context.getString(R.string.Manager_ModuleVersionsTable_VersionColumn),version);
+            long id = sqLiteHelper.insertToDB(context.getString(R.string.Manager_ModuleVersionsTable),cv);
+            if(id == -1)
+                throw new SQLDataException("Row could not be inserted");
+        }
+        else
+            throw new InternalError("Duplicate module in modules table - RIP Project");
     }
 
     private boolean newVersion(Module module)
