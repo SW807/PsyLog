d30593efcc6454f3e8c63da185f1c6ba98419864
Søren Skibsted Als <s_s_a_92@hotmail.com>
2015-03-09 14:20:07 +0100

    Dependencies virker

--- app/src/main/java/dk/aau/cs/psylog/psylog/SettingsFragment.java
+++ app/src/main/java/dk/aau/cs/psylog/psylog/SettingsFragment.java
@@ -23,6 +23,7 @@ import android.support.v7.widget.LinearLayoutCompat;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -32,9 +33,16 @@ import android.view.Window;
 import android.widget.ImageView;
 
 
+import java.net.DatagramPacket;
 import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.prefs.PreferenceChangeListener;
 
+import dk.aau.cs.psylog.generated.Dependency;
 import dk.aau.cs.psylog.generated.Module;
 
 /**
@@ -48,40 +56,101 @@ import dk.aau.cs.psylog.generated.Module;
  * href="http://developer.android.com/guide/topics/ui/settings.html">Settings
  * API Guide</a> for more information on developing a Settings UI.
  */
-public class SettingsFragment extends PreferenceActivity{
+public class SettingsFragment extends PreferenceActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        requestWindowFeature(Window.FEATURE_ACTION_BAR);
-        requestWindowFeature(Window.FEATURE_OPTIONS_PANEL);
         super.onCreate(savedInstanceState);
+        initSettings();
+    }
+
+    private void initSettings() {
         PreferenceScreen root = getPreferenceManager().createPreferenceScreen(this);
 
         PreferenceCategory preferenceCategory = new PreferenceCategory(this);
         preferenceCategory.setTitle("Analyse Moduler");
         root.addPreference(preferenceCategory);
 
-
         JSONParser parser = new JSONParser(this);
         ArrayList<Module> modules = parser.parse();
-        for(Module module : modules)
-        {
-            preferenceCategory.addPreference(makePreference(module.getName(), module.getName(), "some summa", true, false));
+
+        final HashMap<String, CheckBoxPreference> dicModules = new HashMap<String, CheckBoxPreference>();
+
+        for (Module module : modules) {
+            dicModules.put(module.getName(), makePreference(module.getName(), module.getName(), "some summa", true, false));
         }
 
+        for (Module module : modules) {
+            ArrayList<Module> dependencies = new ArrayList<Module>();
+            final List<Pair<Module, Set<Dependency>>> dependencySet = new ArrayList<Pair<Module, Set<Dependency>>>();
+            for (Module module2 : modules) {
+                if (!module.getName().equals(module2.getName())) {
+                    Set<Dependency> res = containsString(module2.getDependencies(), module.getName());
+                    if (res != null) {
+                        dependencySet.add(new Pair<Module, Set<Dependency>>(module2, res));
+                    }
+                }
+            }
+            Preference.OnPreferenceChangeListener listener = new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    if (newValue.toString().equals("true")) {
+                        for (Pair<Module, Set<Dependency>> setDP : dependencySet) {
+                            dicModules.get(setDP.first.getName()).setEnabled(true);
+                        }
+                    } else {
+                        for (Pair<Module, Set<Dependency>> setDP : dependencySet) {
+                            boolean checked = false;
+                            for (Dependency dp : setDP.second) {
+                                if (dicModules.get(dp.getName()).isChecked() && !dp.getName().equals(preference.getKey())) {
+                                    checked = true;
+                                    break;
+                                }
+                            }
+                            dicModules.get(setDP.first.getName()).setEnabled(checked);
+                            dicModules.get(setDP.first.getName()).setChecked(checked);
+                            Preference pref = new Preference(getCon());
+                            pref.setKey(setDP.first.getName());
+                            dicModules.get(setDP.first.getName()).getOnPreferenceChangeListener().onPreferenceChange(pref, checked);
+                        }
+                    }
+                    return true;
+                }
+            };
+            dicModules.get(module.getName()).setOnPreferenceChangeListener(listener);
+
+        }
+
+        for (CheckBoxPreference value : dicModules.values()) {
+            preferenceCategory.addPreference(value);
+        }
         this.setPreferenceScreen(root);
 
     }
 
-    public CheckBoxPreference makePreference(String key, String title, String summary, boolean enabled, boolean checked)
+    public Context getCon()
     {
+        return this;
+    }
+
+    private Set<Dependency> containsString(List<Set<Dependency>> dependencies, String name) {
+        for (Set<Dependency> dpSet : dependencies) {
+            for (Dependency dp : dpSet) {
+                if (dp.getName().equals(name))
+                    return dpSet;
+            }
+        }
+        return null;
+    }
+
+    private CheckBoxPreference makePreference(String key, String title, String summary, boolean enabled, boolean checked) {
         CheckBoxPreference checkBoxPreference = new CheckBoxPreference(this);
         checkBoxPreference.setKey(key);
         checkBoxPreference.setTitle(title);
         checkBoxPreference.setSummary(summary);
         checkBoxPreference.setEnabled(enabled);
         checkBoxPreference.setChecked(checked);
-        return  checkBoxPreference;
+        return checkBoxPreference;
     }
 
 }
