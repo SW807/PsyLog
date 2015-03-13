c549decd2e1cf5c773e4a65af0d78aafa9c472f0
Bruno Thalmann <mr.thalmann@gmail.com>
2015-03-13 10:24:17 +0100

    class for static final fields in psylog

---
+++ app/src/main/java/dk/aau/cs/psylog/psylog/PsyLogConstants.java
@@ -0,0 +1,9 @@
+package dk.aau.cs.psylog.psylog;
+
+/**
+ * Created by - on 13-03-2015.
+ */
+public final class PsyLogConstants {
+    // Packagename: dk.aau.cs.psylog. '.' is replaced by '_' for avoiding collision with SQLite syntax
+    public static final String PACKAGE_NAME = "dk_aau_cs_psylog_";
+}
