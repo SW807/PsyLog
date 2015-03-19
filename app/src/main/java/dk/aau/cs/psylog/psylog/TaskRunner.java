ee0c0e8db74665c38755ddae9163f9b893d0ab22
MikaelEC <mikaelec@gmail.com>
2015-03-19 11:25:57 +0100

    Extracted task type and value from module

--- app/src/main/java/dk/aau/cs/psylog/psylog/TaskRunner.java
+++ app/src/main/java/dk/aau/cs/psylog/psylog/TaskRunner.java
@@ -7,9 +7,14 @@ import android.os.IBinder;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
 
 import dk.aau.cs.psylog.PsyLogConstants;
+import dk.aau.cs.psylog.data_access_layer.generated.AnalysisModule;
 import dk.aau.cs.psylog.data_access_layer.generated.Module;
+import dk.aau.cs.psylog.data_access_layer.generated.SensorModule;
+
+import static dk.aau.cs.psylog.psylog.Task.*;
 
 public class TaskRunner extends Service {
     private ArrayList<Task> tasks;
@@ -28,6 +33,20 @@ public class TaskRunner extends Service {
 
     private void updateTaskTime(Task task)
     {
+        Module m = task.getModule();
+        dk.aau.cs.psylog.data_access_layer.generated.Task.Type taskType;
+        String taskValue;
+        if (m instanceof SensorModule) {
+            taskType = ((SensorModule) m).getTask().getType();
+            taskValue = ((SensorModule) m).getTask().getValue();
+        }
+        else if (m instanceof AnalysisModule) {
+            taskType = ((AnalysisModule) m).getTask().getType();
+            taskValue = ((AnalysisModule) m).getTask().getValue();
+        }
+
+        Date newTime;
+
         //task.setTime(task.getModule().);
         sortAfterRunningTime();
     }
