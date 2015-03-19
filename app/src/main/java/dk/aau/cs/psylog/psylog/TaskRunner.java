3bf5488364c8e3618dcb6579753bc4d50a1156bd
Bruno Thalmann <mr.thalmann@gmail.com>
2015-03-19 09:30:46 +0100

    added manipulation methods of the list of tasks tot the task runner

--- app/src/main/java/dk/aau/cs/psylog/psylog/TaskRunner.java
+++ app/src/main/java/dk/aau/cs/psylog/psylog/TaskRunner.java
@@ -6,6 +6,10 @@ import android.os.IBinder;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
+
+import dk.aau.cs.psylog.PsyLogConstants;
+import dk.aau.cs.psylog.data_access_layer.generated.Module;
 
 public class TaskRunner extends Service {
     private ArrayList<Task> tasks;
@@ -19,12 +23,28 @@ public class TaskRunner extends Service {
     }
 
     private void sortAfterRunningTime(){
-        Collections.sort(this.tasks, ;
+        Collections.sort(this.tasks, new TaskComparator());
+    }
 
+    private void updateTaskTime(Task task)
+    {
+        //task.setTime(task.getModule().);
+        sortAfterRunningTime();
+    }
+
+    private void startTask(Task task){
+        ServiceHelper.startService(ServiceHelper.getProcessName(task.getModule()), this);
     }
 
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    private class TaskComparator implements Comparator<Task>{
+        @Override
+        public int compare(Task lhs, Task rhs) {
+            return lhs.getTime().compareTo(rhs.getTime());
+        }
     }
 }
