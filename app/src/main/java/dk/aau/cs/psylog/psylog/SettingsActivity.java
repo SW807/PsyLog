ee38417c96e9f2a23d85240597d0938438e8a56c
Mathias Winde <Mapede11@student.aau.dk>
2015-03-24 15:32:00 +0100

    Lave Module til ModuleNode om fra et HashMap til et List med pairs i, grunden til dette er fordi Mikael ikke kan lave en god nok hashcode funktion i de generede funktioner af Pojo ting ting

--- app/src/main/java/dk/aau/cs/psylog/psylog/SettingsActivity.java
+++ app/src/main/java/dk/aau/cs/psylog/psylog/SettingsActivity.java
@@ -8,11 +8,15 @@ import android.preference.PreferenceActivity;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
 import android.util.Log;
+import android.util.Pair;
 
 import java.util.ArrayList;
+import java.util.Dictionary;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
 import dk.aau.cs.psylog.PsyLogConstants;
 import dk.aau.cs.psylog.data_access_layer.DependencyGraph.ErrorNode;
@@ -28,7 +32,7 @@ import dk.aau.cs.psylog.data_access_layer.generated.SensorModule;
 public class SettingsActivity extends PreferenceActivity {
     private ArrayList<Module> modules;
     private HashMap<String, Module> stringModuleHashMap = new HashMap<>();
-    private HashMap<Module, ModuleNode> moduleModuleNodeHashMap = new HashMap<>();
+    private List<Pair<Module, ModuleNode>> moduleModuleNodeHashMap = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -48,16 +52,16 @@ public class SettingsActivity extends PreferenceActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        for (Map.Entry<Module, ModuleNode> entry : moduleModuleNodeHashMap.entrySet()) {
+        for (Pair<Module, ModuleNode> entry : moduleModuleNodeHashMap) {
             String type = "";
-            if (entry.getKey() instanceof AnalysisModule)
+            if (entry.first instanceof AnalysisModule)
                 type = "analysis.";
-            else if (entry.getKey() instanceof SensorModule)
+            else if (entry.first instanceof SensorModule)
                 type = "sensor.";
-            if (entry.getValue().getChecked())
-                ServiceHelper.startService(PsyLogConstants.DOMAIN_NAME + type + entry.getKey().getName(), this);
+            if (entry.second.getChecked())
+                ServiceHelper.startService(PsyLogConstants.DOMAIN_NAME + type + entry.first.getName(), this);
             else
-                ServiceHelper.stopService(PsyLogConstants.DOMAIN_NAME + type + entry.getKey().getName(), this);
+                ServiceHelper.stopService(PsyLogConstants.DOMAIN_NAME + type + entry.first.getName(), this);
         }
     }
 
@@ -67,7 +71,7 @@ public class SettingsActivity extends PreferenceActivity {
     private void FillHashMaps() {
         for (Module m : modules) {
             stringModuleHashMap.put(m.getName(), m);
-            moduleModuleNodeHashMap.put(m, new ModuleNode(makePreference(m.getName(), m.getName(), m.get_description(), false, false)));
+            moduleModuleNodeHashMap.add(new Pair<>(m, new ModuleNode(makePreference(m.getName(), m.getName(), m.get_description(), false, false))));
         }
     }
 
@@ -83,33 +87,40 @@ public class SettingsActivity extends PreferenceActivity {
                         if (stringModuleHashMap.containsKey(dependency.getName())) {
                             Module moduleToAdd = stringModuleHashMap.get(dependency.getName());
                             if (moduleToAdd.get_version().equals(dependency.getVersion()))
-                                nodesToBeAdded.add(moduleModuleNodeHashMap.get(moduleToAdd));
+                                nodesToBeAdded.add(getModuleNode(moduleToAdd));
                             else
                                 nodesToBeAdded.add(new ErrorNode(ModuleEnum.WRONG_VERSION));
                         } else
                             nodesToBeAdded.add(new ErrorNode(ModuleEnum.NOT_INSTALLED));
                     }
-                    moduleModuleNodeHashMap.get(m).addDependency(nodesToBeAdded);
+                    getModuleNode(m).addDependency(nodesToBeAdded);
                 }
             }
         }
     }
 
+    private ModuleNode getModuleNode(Module module){
+        for(Pair<Module, ModuleNode> pair : moduleModuleNodeHashMap){
+            if(pair.first.getName().equals(module.getName()))
+                return pair.second;
+        }
+        return null;
+    }
     /**
      * Sets the OnPreferenceChangeListener for modules
      */
     private void SetOnPreferenceChangeListenerForModules() {
-        for (final Map.Entry<Module, ModuleNode> entry : moduleModuleNodeHashMap.entrySet()) {
-            entry.getValue().setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+        for (final Pair<Module, ModuleNode> entry : moduleModuleNodeHashMap) {
+            entry.second.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
                 @Override
                 public boolean onPreferenceChange(Preference preference, Object newValue) {
-                    entry.getValue().setChecked((boolean) newValue);
-                    for (INode iNode : entry.getValue().getParents()) {
+                    entry.second.setChecked((boolean) newValue);
+                    for (INode iNode : entry.second.getParents()) {
                         if (iNode instanceof ModuleNode) {
                             ModuleNode moduleNode = (ModuleNode) iNode;
                             moduleNode.setEnabled(moduleNode.isValid().equals(ModuleEnum.VALID));
                             Preference pref = new Preference(getSettingsContext());
-                            pref.setKey(entry.getKey().getName());
+                            pref.setKey(entry.first.getName());
                             moduleNode.callOnPreferenceChange(pref, false);
                         }
                     }
@@ -128,17 +139,17 @@ public class SettingsActivity extends PreferenceActivity {
             String modName = entry.getKey().substring(entry.getKey().lastIndexOf('.') + 1);
             pref.setKey(modName);
             try {
-                moduleModuleNodeHashMap.get(stringModuleHashMap.get(modName)).setChecked(entry.getValue());
+                getModuleNode(stringModuleHashMap.get(modName)).setChecked(entry.getValue());
             }
             catch(NullPointerException e){
             }
         }
 
-        for (ModuleNode m : moduleModuleNodeHashMap.values()) {
-            boolean enabled = m.isValid().equals(ModuleEnum.VALID);
-            m.setEnabled(enabled);
+        for (Pair<Module,ModuleNode> m : moduleModuleNodeHashMap) {
+            boolean enabled = m.second.isValid().equals(ModuleEnum.VALID);
+            m.second.setEnabled(enabled);
             if (!enabled)
-                m.setChecked(false);
+                m.second.setChecked(false);
         }
     }
 
@@ -155,11 +166,11 @@ public class SettingsActivity extends PreferenceActivity {
 
         //Makes the topological sort of checkBoxPrefenrences
         HashMap <Integer, ArrayList<ModuleNode>> sortedModuleNodes = new HashMap<>();
-        for(ModuleNode value : moduleModuleNodeHashMap.values()){
-            int curMaxLevel = value.getMaxLevel();
+        for(Pair<Module,ModuleNode> value : moduleModuleNodeHashMap){
+            int curMaxLevel = value.second.getMaxLevel();
             if(!sortedModuleNodes.containsKey(curMaxLevel))
                 sortedModuleNodes.put(curMaxLevel,new ArrayList<ModuleNode>());
-            sortedModuleNodes.get(curMaxLevel).add(value);
+            sortedModuleNodes.get(curMaxLevel).add(value.second);
         }
 
         for(int i = 1; i <= sortedModuleNodes.values().size(); i++){
