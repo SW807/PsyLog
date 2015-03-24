5258bc2d650b78d2d8010ba24ea71eeb758b523a
Lasse Vang Gravesen <gravesenlasse@gmail.com>
2015-03-24 10:15:37 +0100

    Clean up MainActivity for adding graph views.

--- app/src/main/java/dk/aau/cs/psylog/psylog/MainActivity.java
+++ app/src/main/java/dk/aau/cs/psylog/psylog/MainActivity.java
@@ -1,27 +1,14 @@
 package dk.aau.cs.psylog.psylog;
 
-import android.app.ActionBar;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.TypedValue;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.Window;
-import android.widget.ArrayAdapter;
-import android.widget.FrameLayout;
 import android.widget.LinearLayout;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-import com.github.mikephil.charting.data.BarData;
-
-import java.util.ArrayList;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -34,27 +21,15 @@ public class MainActivity extends ActionBarActivity {
         Manager manager = new Manager(this);
         manager.updateModules();
 
-      //  ListView listView = (ListView)findViewById(R.id.lstview);
-        adapter = new CustomAdapter(this, android.R.layout.simple_list_item_1, listItems);
-
-        //adapter = new ArrayAdapter<View>(this, android.R.layout.simple_list_item_1, listItems);
-        //listView.setAdapter(adapter);
-
         ViewLoader viewLoader = new ViewLoader(this);
         View textView = viewLoader.getView("dk.aau.cs.psylog.view.stepcountview", "dk.aau.cs.psylog.view.stepcountview.HelloChartStepView", "HelloChartStepView");
         LinearLayout layout = (LinearLayout)findViewById(R.id.MainActivityLayout);
         View textView2 = viewLoader.getView("dk.aau.cs.psylog.view.stepcountview", "dk.aau.cs.psylog.view.stepcountview.HelloChartStepView2", "HelloChartStepView2");
-        View textView3 = viewLoader.getView("dk.aau.cs.psylog.view.stepcountview", "dk.aau.cs.psylog.view.stepcountview.HelloChartStepView3", "HelloChartStepView3");
         int height = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 150, getResources().getDisplayMetrics());
         int width = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 300, getResources().getDisplayMetrics());
         layout.addView(textView, width, height);
         layout.addView(textView2, width, height);
-        layout.addView(textView3, width, height);
     }
-    ArrayAdapter<View> adapter;
-    ListAdapter adapter2;
-    ArrayList<View> listItems = new ArrayList<>();
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
